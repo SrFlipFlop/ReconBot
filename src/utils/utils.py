6c75ef042e485a3e2ffb825ecc95df0c64be045e
srflipflop <srflipflop@srflipflop.srflipflop.net>
2018-01-17 18:40:20 +0100

    add load config

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,16 @@
+from os.path import dirname, realpath
+from logging import warning
+from json import load
+from sys import exit
+
 def load_config():
-    pass
+    try:
+        path = "{0}/bot_config.json".format(dirname(realpath(__file__)))
+		with open(path, 'r') as conf:
+			return load(conf)
+	except:
+		warning('Configuration not found')
+		exit()
 
 def store_config():
     pass
