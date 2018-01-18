7a38cdfff1fd0a7e5c133a43a95b3e190a24466a
root <root@localhost.localdomain>
2018-01-19 00:40:06 +0100

    add cron jobs

--- src/manager.py
+++ src/manager.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from crontab import CronTab 
-#https://code.tutsplus.com/tutorials/managing-cron-jobs-using-python--cms-28231
+from crontab import CronTab #https://code.tutsplus.com/tutorials/managing-cron-jobs-using-python--cms-28231
+
+def add_corn_task():
+    cron = CronTab(user='user')
+    job = cron.new(command='python /home/jay/writeDate.py')
+    job.minute.every(1)
+    cron.write()
+
+def update_cron_task():
+    pass
+
+def remove_cron_task():
+    pass
