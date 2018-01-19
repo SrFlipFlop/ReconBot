1d671f9f69679847fa112bceed4dbf6c43a9e8bb
srflipflop <srflipflop@srflipflop.srflipflop.net>
2018-01-19 15:30:23 +0100

    fast commit no time for comments :3

--- src/attacker.py
+++ src/attacker.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#TODO: launch scripts to enumerate info form the services
--- src/storage.py
+++ src/storage.py
@@ -4,6 +4,20 @@ from sqlite3 import connect
 #Assets: | id | name | url | platform | android | ios | reward | scope |
 #Scope: | asset | url | time |
 
+def create_db(db_path):
+    connection = connect()
+    c = connection.cursor()
+    
+    c.execute("CREATE TABLE platforms (name text, url text)")
+    c.execute("CREATE TABLE assets (id text, name text, hash text, timestamp real)")
+
+    
+    connection.commit()
+    connection.close()
+
+def connect_db(db_path)
+    return connect(db_path)
+
 def save_platforms_db(platforms):
     pass
 
